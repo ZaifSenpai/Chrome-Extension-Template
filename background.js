5ed964c8921e3ecc5af60915718ea909a40fe8ea
Zaif Senpai <escbutt@gmail.com>
2020-02-21 08:21:43 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+//// To limit for specific host
+// chrome.runtime.onInstalled.addListener(function () {
+//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//         chrome.declarativeContent.onPageChanged.addRules([{
+//             conditions: [new chrome.declarativeContent.PageStateMatcher({
+//                 pageUrl: { hostContains: 'google.com' },
+//             })
+//             ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//         }]);
+//     });
+// });
+
 chrome.runtime.onInstalled.addListener(function () {
     new chrome.declarativeContent.ShowPageAction();
 });
