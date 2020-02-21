c9037d19df36adb84daccd51ba86a7fc3f3fb047
Zaif Senpai <escbutt@gmail.com>
2020-02-21 08:21:10 +0500

    Added listener for commands

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
 chrome.runtime.onInstalled.addListener(function () {
     new chrome.declarativeContent.ShowPageAction();
 });
+
+chrome.commands.onCommand.addListener(function (command) {
+    if (command == "next") {
+        // do something
+    }
+    else if (command == "cancel") {
+        // do something
+    }
+});
