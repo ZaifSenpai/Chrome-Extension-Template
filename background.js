ac15944b75ca3424de4bab31d0039365dba7cc93
Zaif Senpai <escbutt@gmail.com>
2020-08-06 00:14:41 +0500

    Removed declarativeContent

--- background.js
+++ background.js
@@ -1,20 +1,3 @@
-//// To limit for specific host
-// chrome.runtime.onInstalled.addListener(function () {
-//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-//         chrome.declarativeContent.onPageChanged.addRules([{
-//             conditions: [new chrome.declarativeContent.PageStateMatcher({
-//                 pageUrl: { hostContains: 'google.com' },
-//             })
-//             ],
-//             actions: [new chrome.declarativeContent.ShowPageAction()]
-//         }]);
-//     });
-// });
-
-chrome.runtime.onInstalled.addListener(function () {
-    new chrome.declarativeContent.ShowPageAction();
-});
-
 chrome.commands.onCommand.addListener(function (command) {
     if (command == "next") {
         // do something
